cddcbff32c521fe3d00ab795ae07f9f42aa76e41
Firoz Mahmud Rizvi <firoz@webalive.com.au>
2024-10-02 21:58:33 +1000

    Complete Setup & Call From Database

---
+++ djangoSDK/service/order_service.py
@@ -0,0 +1,25 @@
+from ab_py.common.ab_exception import ABException
+from ab_py.exsited.exsited_sdk import ExsitedSDK
+from service.exsited_service import ExsitedService
+from tests.common.common_data import CommonData
+
+
+class OrderService:
+    def __init__(self, exsited_service: ExsitedService):
+
+        self.exsited_service = exsited_service
+
+    def get_charge_item_uuid_by_order_id(self, order_id, item_name):
+        sdk = self.exsited_service.get_sdk()
+        try:
+            response = sdk.order.details(id=order_id)
+            print(response)
+            if response.order:
+                for line in response.order.lines:
+                    if line.itemName == item_name:
+                        return line.chargeItemUuid
+            return None
+        except ABException as ab:
+            error_code = None
+            if ab.get_errors() and "errors" in ab.raw_response:
+                error_code = ab.raw_response["errors"][0].get("code", None)
---
+++ djangoSDK/service/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse, JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from ab_py.common.ab_exception import ABException
+from ab_py.exsited.exsited_sdk import ExsitedSDK
+from tests.common.common_data import CommonData
+from .utils import fetch_call_usage, fetch_message_usage
+
+
+@api_view(['GET'])
+def call_usage(request):
+    call_usage_data = fetch_call_usage()
+
+    # Return the data as JSON
+    return JsonResponse({"status": "success", "usage:": call_usage_data}, safe=False)
+
+
+@api_view(['GET'])
+def message_usage(request):
+    message_usage_data = fetch_message_usage()
+    return JsonResponse(message_usage_data, safe=False)
+
+
